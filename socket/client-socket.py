285e86715b6f0af4d3b1c67e84bc66abc6aff939
emcristrel <94045340+emcristrel@users.noreply.github.com>
2022-11-21 10:29:08 +0100

    création socket
    création de sockets + boucle pour l'envoi de message, quelque bugs encore

--- socket/client-socket.py
+++ socket/client-socket.py
@@ -1,7 +1,24 @@
 import socket
 
-client_socket = socket.socket()
-client_socket.connect(('localhost', 10000))
-client_socket.send(message.encode())
-data = client_socket.recv(1024).decode()
-client_socket.close()
+message = "rien"
+
+client_socket = socket.socket() #Création de la socket
+client_socket.connect(('localhost', 10000)) #Connexion au host et au port host = "@IP" -> localhost
+print("Connexion établie...")
+
+menu = input("Menu général, que voulez-vous faire ?\n1. Envoyer un message\n2. Quitter\n")
+
+
+if menu == "1" : # Si demande d'envoi de message, ouverture d'une boucle
+
+    while message != "quit": #Création boucle pour laisser connexion ouverte tant que != de quit
+        message = input("que voulez-vous envoyer : ")
+        print("Message envoyé... en attente d'une réponse")
+        client_socket.send(message.encode()) #Envoi de données
+        data = client_socket.recv(1024).decode() #Réception de données
+        print(f"Message reçu : {data}")
+        code = input("voulez vous quitter ? (y/n) : ")
+
+
+if code == "y" or menu =="2" or message == "quit":
+    client_socket.close() #Fermeture de la communication
--- socket/serv-socket.py
+++ socket/serv-socket.py
@@ -1,10 +1,24 @@
 import socket
+message = "rien"
 
 server_socket = socket.socket() #création de socket
 server_socket.bind(('localhost', 10000)) #association du host(@IP)
 print("Serveur démarré...")
-server_socket.listen(1)
-conn, address= server_socket.accept()
-data = conn.recv(1024).decode()
-conn.send(reply.encode())
-conn.clos()
+server_socket.listen(1) #Attente de la connexion et nombre de connexion établie
+
+connexion = input("Menu général, que voulez-vous faire ?\n1. Etablir connexion avec client \n2. Quitter\n")
+
+while connexion ==1:
+    conn, address= server_socket.accept() #Etablissement de la communication
+    
+    while message != "quit":
+        data = conn.recv(1024).decode() #Réception de données
+        print(f"Message reçu : {data}")
+        message = input("que voulez-vous envoyer : ")
+        print("Message envoyé... en attente d'une réponse")
+        conn.send(reply.encode()) #Envoi de données
+    
+    conn.close() #Fermeture de la communication
+    sortie = input("voulez vous quitter ? (y/n) : ")
+    if sortie == "y" or message == "quit":
+        connexion == "0" 
