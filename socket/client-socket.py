a88be4becf71e9a40394fb61fb9eb28b9d31abb7
emcristrel <94045340+emcristrel@users.noreply.github.com>
2022-11-21 12:56:30 +0100

    Update client-socket.py

--- socket/client-socket.py
+++ socket/client-socket.py
@@ -1,24 +1,21 @@
 import socket
 
-message = "rien"
 
 client_socket = socket.socket() #Création de la socket
 client_socket.connect(('localhost', 10000)) #Connexion au host et au port host = "@IP" -> localhost
 print("Connexion établie...")
 
+message = "" #message saisit par utilisateur et envoyé
+data = "" #ce qui est reçu par l'utilisateur du serveur
 menu = input("Menu général, que voulez-vous faire ?\n1. Envoyer un message\n2. Quitter\n")
 
 
 if menu == "1" : # Si demande d'envoi de message, ouverture d'une boucle
 
-    while message != "quit": #Création boucle pour laisser connexion ouverte tant que != de quit
+    while message != "quit" and message != "bye" and data != "bye" and data != "quit": #Création boucle pour laisser connexion ouverte tant que != de quit ou bye
         message = input("que voulez-vous envoyer : ")
         print("Message envoyé... en attente d'une réponse")
         client_socket.send(message.encode()) #Envoi de données
         data = client_socket.recv(1024).decode() #Réception de données
         print(f"Message reçu : {data}")
-        code = input("voulez vous quitter ? (y/n) : ")
-
-
-if code == "y" or menu =="2" or message == "quit":
     client_socket.close() #Fermeture de la communication
